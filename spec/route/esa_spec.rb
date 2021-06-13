cd87f632f6e7e1b35b2268d86450c2f3d952f233
Akihiko Horiuchi <12ff5b8@gmail.com>
2021-06-13 21:08:30 +0900

    :+1: Add test patterns for Jinaki::Route::Esa

--- spec/route/esa_spec.rb
+++ spec/route/esa_spec.rb
@@ -2,13 +2,26 @@ describe Jinaki::Route::Esa do
   context '/esa/events' do
     let(:path) { '/esa/events' }
 
-    context 'post' do
-      before { allow_any_instance_of(Jinaki::Endpoint::Esa).to receive(:post_events) }
-      subject { post(path) }
+		[
+		  [:get, nil, 404],
+		  [:post, :post_events, 204],
+		  [:put, nil, 404],
+		  [:patch, nil, 404],
+		  [:delete, nil, 404]
+		].each do |method, action, status|
+      context method do
+        before { allow_any_instance_of(Jinaki::Endpoint::Esa).to receive(action) if action }
+        subject { send(method, path) }
 
-      it 'status 204' do
-        expect_any_instance_of(Jinaki::Endpoint::Esa).to receive(:post_events).once
-        expect(subject.status).to eq 204
+				it "##{action} is called", if: action do
+          expect_any_instance_of(Jinaki::Endpoint::Esa).to receive(action).once
+          subject
+        end
+
+
+        it "status #{status}" do
+          expect(subject.status).to eq status
+        end
       end
     end
   end
