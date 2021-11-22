0aec5308351178853c1eda34fd420f7d55688e23
Akihiko Horiuchi <12ff5b8@gmail.com>
2021-11-22 11:34:25 +0900

    :recycle: Make methods in helpers private

--- lib/jinaki/helper/esa.rb
+++ lib/jinaki/helper/esa.rb
@@ -4,6 +4,8 @@ module Jinaki
       ESA_ICON = 'https://assets.esa.io/assets/apple-touch-icon-133ed3152feb59153eeaf8849cf1cad0b4447e95f359ce7f621f75fad49d1512.png'.freeze
       ESA_COLOR = '#0a9b94'.freeze
 
+      private
+
       def esa_client
         @esa_client ||= ::Esa::Client.new(access_token: ENV['ESA_ACCESS_TOKEN'], current_team: ENV['ESA_CURRENT_TEAM'])
       end
--- lib/jinaki/helper/slack.rb
+++ lib/jinaki/helper/slack.rb
@@ -1,6 +1,8 @@
 module Jinaki
   module Helper
     module Slack
+      private
+
       def slack_webhook
         @slack_webhook ||= ::Slack::Incoming::Webhooks.new(ENV['SLACK_WEBHOOK_URL'])
       end
